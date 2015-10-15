4fbf3267ae4b3df06c092b1ab92b49972f3e86af
erichermann13 <erichermann13@gmail.com>
2015-10-15 15:17:35 -0700

    got players function working

--- getPlayers.py
+++ getPlayers.py
@@ -4,6 +4,7 @@ import csv
 from datetime import datetime, date
 import pandas as pd
 import numpy as np
+import html5lib
 
 # Note: this code was strongly borrowed from http://www.danielforsyth.me/exploring_nba_data_in_python/
 
@@ -30,28 +31,42 @@ def get_players(players, team_name):
 	array[:] = np.nan
 	for i, player in enumerate(players):
 		cols = player.find_all('td')
+		print "#########################################################"
+		# print cols[0].text
+		# break
 		array[i, 0] = cols[0].text.split(',')[0]
-        for j in range(1, len(headers) + 1):
-            if not cols[1].text.startswith('DNP'):
-                array[i, j] = cols[j].text
+		print array[i, 0]
+		for j in range(1, len(headers) + 1):
+			if not cols[1].text.startswith('DNP'):
+				array[i, j] = cols[j].text
+				print cols[j].text
+	print array
+	print "9999999999999999999999999999999999999999999999999999"
 	frame = pd.DataFrame(columns=columns)
 	for x in array:
+		print x
 		line = np.concatenate(([index, team_name], x)).reshape(1,len(columns))
 		new = pd.DataFrame(line, columns=frame.columns)
 		frame = frame.append(new)
 	return frame
 
-for index, row in games[:3].iterrows():
+for index, row in games[:10].iterrows():
 # for index, row in games.iterrows():
     print(index)
-    request = requests.get(BASE_URL.format(index))
-    table = BeautifulSoup(request.text).find('table', class_='mod-data')
+    print row
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    request = requests.get(BASE_URL.format(row['id']))
+    print request.url
+    table = BeautifulSoup(request.text, 'html5lib').find('table', class_='mod-data')
+    # print table.prettify()
     heads = table.find_all('thead')
     bodies = table.find_all('tbody')
 
     team_1 = heads[0].th.text
+    print team_1
     team_1_players = bodies[0].find_all('tr') + bodies[1].find_all('tr')
     team_1_players = get_players(team_1_players, team_1)
+    print team_1_players
     players = players.append(team_1_players)
     
     team_2 = heads[3].th.text
