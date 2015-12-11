44ebc1e7f9dd15fc696d2e8e25f1194b4ba187f2
Mikaela Grace <gracemikaela@gmail.com>
2015-12-10 18:02:27 -0800

    pandas stuff in evaluate_csp.csv

--- evaluate_csp.py
+++ evaluate_csp.py
@@ -8,12 +8,13 @@ import pandas as pd
 
 def main():
     #make_teams_for_all_days()
-    #our_scores = calculate_team_score(our_teams_csv)
     their_scores = calculate_team_score(their_teams_csv)
-    our_teams = pd.read_csv(our_teams_csv)
-    our_scores = our_teams.TotalPoints
-    print "our scores was: {}".format(our_scores)
-    print "their scores was: {}".format(their_scores)
+    scores = pd.read_csv(our_teams_csv)
+    our_scores =scores.TotalPoints.tolist()
+    scores['FantasyPoints'] = their_scores
+
+    print "our scores was: {}".format(len(our_scores))
+    print "their scores was: {}".format(len(their_scores))
 
     #do analysis about what the difference is!
 
@@ -25,7 +26,7 @@ def calculate_team_score(teams_filename):
         teams_reader = csv.reader(teams_file)
         header = teams_reader.next()
         for team in teams_reader:
-            print "team was {}".format(team)
+    #        print "team was {}".format(team)
             date = team[0]
             if not date: continue
             total_score = 0
@@ -40,7 +41,7 @@ def calculate_team_score(teams_filename):
 def make_teams_for_all_days():
     with open(our_teams_csv, 'w+') as f:
         wr = csv.writer(f)
-        data_files = ['SalariesData/DKSalaries11.{}.2015.csv'.format(i) for i in range(11, 14) + range(16,20) + [30]]
+        data_files = ['SalariesData/DKSalaries11.{}.2015.csv'.format(i) for i in range(11, 14) + range(16,21) + [30]]
         data_files += ['SalariesData/DKSalaries12.{}.2015.csv'.format(i) for i in range(1,7)]
         print "number of successful files was {}".format(len(data_files))
         for data_file in data_files:
